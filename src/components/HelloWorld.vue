83eb75c494d0ad9b22178a20d5fa58c9f9ad6416
uynilo9 <uynilo9@outlook.com>
2024-01-31 23:12:26 +0800

    Reorder the classes in HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
     <h1
         class="
             font-500
-            @dark:text-white:90 @light:text-black:90
+            @light:text-black:90 @dark:text-white:90
             selection:@dark:bg-white:90 selection:@dark:text-black:90
             selection:@light:bg-black:90 selection:@light:text-white:90
         "
