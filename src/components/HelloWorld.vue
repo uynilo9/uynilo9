e6b23004fc0f42bf3c10eb4563e0e0f2c5703e5a
uynilo9 <uynilo9@outlook.com>
2024-02-01 22:26:39 +0800

    Setup the shortcuts in UnoCSS

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,5 @@
 <template>
     <h1
-        class="
-            font-500
-            @light:text-black:90 @dark:text-white:90
-            selection:@dark:bg-white:90 selection:@dark:text-black:90
-            selection:@light:bg-black:90 selection:@light:text-white:90
-        "
+        class="text-default font-500"
     >Hello World</h1>
 </template>
--- uno.config.ts
+++ uno.config.ts
@@ -9,5 +9,9 @@ export default unocss.defineConfig({
                 quicksand: 'Quicksand:400,500'
             }
         })
-    ]
+    ],
+    shortcuts: {
+        'body-default': 'flex font-quicksand @light:bg-black:10 @dark:bg-black:90',
+        'text-default': 'font-400 @light:text-black:90 @dark:text-white:90 selection:@dark:bg-white:90 selection:@dark:text-black:90 selection:@light:bg-black:90 selection:@light:text-white:90'
+    }
 });
