4f953ed4f0108642d543dc72a2167e858626a21b
uynilo9 <uynilo9@outlook.com>
2024-02-03 00:04:21 +0800

    Modify the stupid contents and grammar in the README.md file and the About page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <p class="font-400 text-4 lh-normal text-default">
-            Just a normal 15 boy from ROC/Taiwan. I enjoy programming, watch Japenese animes, and play video games. I'll start developing some interesting but tiny programs when there's nothing to do (lmao). I hope one day I could become a Full-stack Programmer (but professional) / Gaming Streamer / Physics Enthusiast (sounds like joking). Let's be friends together :D
+            Just a normal 15 boy from ROC/Taiwan. I enjoy programming, watching animes, and playing video games. I'll start developing some interesting but tiny programs when there's nothing to do (lmao). I hope one day I could become a Full-stack Programmer (but professional) / Gaming Streamer / Physics Enthusiast (sounds like joking). DM me on Discord if you wanna be friends with me :D
         </p>
         <div class="grid">
             <img
