a3c63a713e5408d63d16a150a8af48e1d41372d1
uynilo9 <uynilo9@outlook.com>
2024-02-04 18:38:57 +0800

    Modify the gif in the Can.vue file

--- src/components/Can.vue
+++ src/components/Can.vue
@@ -1,6 +1,6 @@
 <template>
     <img
-        src="@assets/spinning.gif" alt=""
+        src="@assets/spinning.gif" alt="" loading="eager" decoding="sync"
         class="max-w-10 max-h-10 select-none pointer-events-none"
     />
 </template>
