c97b1ccf26a763929f6d8362ad1c0c1d06fdff23
uynilo9 <uynilo9@outlook.com>
2024-09-15 15:38:30 +0800

    Temporarily leave the content in About empty

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,12 +5,13 @@
 <template>
 	<div class="h-inherit grid grid-content-between">
 		<p class="font-400 text-4 lh-normal text-default">
-			Just a normal 15 boy from ROC aka Taiwan. I enjoy programming,
+			I'm thinkging what I'm gonna leave here...
+			<!-- Just a normal 15 boy from ROC aka Taiwan. I enjoy programming,
 			watching animes, and playing video games. I always start developing
 			some interesting but tiny programs when there's nothing to do
 			(lmao). I hope one day I could become a Full-stack Programmer (but
 			professional) / Gaming Streamer / Physics Enthusiast (sounds like
-			joking but I'm kinda serious).
+			joking but I'm kinda serious). -->
 		</p>
 		<div
 			class="flex flex-row flex-wrap items-center justify-between"
